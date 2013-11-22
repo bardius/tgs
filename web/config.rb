7dcb646f288ce027e801ab8a37b181e09bfb644c
bardius <george@bardis.info>
2013-11-22 18:51:45 +0000

    migrating to foundation 5

--- web/config.rb
+++ web/config.rb
@@ -1,11 +1,11 @@
-require 'zurb-foundation'
 # Require any additional compass plugins here.
-
+# git config --global url."https://".insteadOf git://
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
 
