132cddae0a231aa52f7d40ecf8cbe67c2722608e
bardius <george.bardis@maynardmalone.com>
2014-05-31 13:27:46 +0100

    automating deployment with Teamcity

--- build/rsync.sh
+++ build/rsync.sh
@@ -12,8 +12,8 @@ echo /usr/bin/rsync -arivzt --delete --no-p --no-o --no-g --exclude-from=$dirRoo
 /usr/bin/rsync -arivzt --delete --no-p --no-o --no-g --exclude-from=$dirRoot/build/exclude.txt --stats $dirRoot/ -e "/usr/bin/ssh -o StrictHostKeyChecking=no -i $dirRoot/$2" bitnami@$3:$4
 
 # Setting folder owners and permissions
-echo /usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo chown -R apache:apache $4"
-/usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo chown -R apache:apache $4"
+echo /usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo chown -R bitnami:bitnami $4"
+/usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo chown -R bitnami:bitnami $4"
 echo /usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo find $4 -type d -print0 | xargs -0 chmod 0755"
 /usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo find $4 -type d -print0 | xargs -0 chmod 0755"
 echo /usr/bin/ssh -i $dirRoot/$2 bitnami@$3 "sudo find $4 -type f -print0 | xargs -0 chmod 0644"
